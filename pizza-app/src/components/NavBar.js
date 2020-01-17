7d140de0e675834f0c2b4d8ed3f36f0efea50fd3
Dan-Vy Le <danvyle85@gmail.com>
2020-01-17 14:49:45 -0800

    added nav bar and example profile

--- pizza-app/src/components/NavBar.js
+++ pizza-app/src/components/NavBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useAuth0 } from "../react-auth0-spa";
+import { Link } from "react-router-dom";
+
 
 const NavBar = () => {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
@@ -11,7 +13,14 @@ const NavBar = () => {
       )}
 
       {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
-    </div>
+
+    {isAuthenticated && (
+      <span>
+        <Link to="/">Home</Link>&nbsp;
+        <Link to="/profile">Profile</Link>
+      </span>
+    )}
+      </div>
   );
 };
 
